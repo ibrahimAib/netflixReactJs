a5a97161b36643155c25684c8fe98a2dcad7214f
“ibrahim <e1415e@gmail.com>
2025-03-15 21:50:44 +0300

    final

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,20 +1,41 @@
-import '../css/MovieCard.css'
+import { useMovieContext } from "../contexts/MovieContext";
+import "../css/MovieCard.css";
 
 function MovieCard({ movie }) {
-  const handleLike = () => {
-    console.log('liked')
-  };
+  const { addToFavorites, removeFromFavorites, isFavorite } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onLikeClick(e) {
+    e.preventDefault();
+    if (!favorite) addToFavorites(movie);
+    else removeFromFavorites(movie.id);
+  }
+
   return (
-    <div className='movie-card'>
-      <div className='movie-poster'>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt="" />
-        <div className='movie-overlay'>
-            <button onClick={handleLike} className='favorite-btn'>♥</button>
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+          alt=""
+        />
+        <div className="movie-overlay">
+          <button
+            onClick={onLikeClick}
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+          >
+            ♥
+          </button>
         </div>
       </div>
 
-      <div className='movie-info'>
-        <h3>{movie.title}</h3>
+      <div className="movie-info">
+        <a
+          rel="stylesheet"
+          href={`https://www.google.com/search?q=${movie.title} مترجم`}
+          target="_blank" 
+        >
+          <h3>{movie.title}</h3>
+        </a>
         <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,26 @@
-import '../css/Favorites.css'
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
+import "../css/Favorites.css";
 function Favorites() {
-  return(
-    <div>
+  const { favorites, isFavorite } = useMovieContext();
+  if (favorites.length != 0) {
+    return (
+      <div>
+        <h1>Favorites:</h1>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+        </div>
+    );
+  }
+  return (
+      <div>
         <h1>No Favorites Movies Yet.</h1>
         <p>Start adding movies to your favorites adn thay will appear hear!!</p>
-    </div>
-  )
+      </div>
+  );
 }
 
 export default Favorites;
